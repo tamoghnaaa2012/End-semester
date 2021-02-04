6183ed3371e5861f6755b18a38467abdd225301f
Tamoghna Pattanayak <42137018+tamoghnaaa2012@users.noreply.github.com>
2021-02-04 13:04:44 +0530

    Add files via upload

--- Q4_exam.py
+++ Q4_exam.py
@@ -18,3 +18,10 @@ h = 0.05
 
 shoting_method(x0, y0, zh0, zl0, xn, yn, h, func1, func2)
 print ("The launch velocity is = the value of Z", )
+
+"""
+For boundary condition, Yn =  45
+After Langarangian interpolation, The value of Z = 33.25985148514848
+And for Z = 33.25985148514848 , The calculated value of Yn = 45.000000000000014
+The launch velocity is = the value of Z
+"""
